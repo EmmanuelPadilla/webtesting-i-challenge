596438968a28b83e1211747c1c8e5897e48ba6f0
Emmanuel Padilla <manolin923@gmail.com>
2020-11-12 08:28:38 -0800

    mvp met

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,26 @@ module.exports = {
 };
 
 function success(item) {
+  if (item.enhancement <20) {
+    return {...item, enhancement: item.enhancement +1}
+  }else{
   return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+
+  if (item.enhancement < 15) {
+    return { ...item, enhancement: item.enhancement - 5 };
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    return { ...item, enhancement: item.enhancement - 10 };
+  } else if (item.enhancement < 16) {
+    return { ...item, enhancement: item.enhancement - 1 };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,5 +1,17 @@
 const {success, fail, repair, get} = require('./enhancer.js');
+const enhancer =require('./enhancer')
 // test away!
+
+const item = {
+    name: "Broad Sword",
+    durability: 0,
+    enhancement: 0
+}
+const item2 = {
+    name: "club",
+    durability: 55,
+    enhancement: 0
+}
 
 describe('learning about tests, assertions and matchers', () => {
     it('sanity check', () => { // the test
@@ -20,11 +32,55 @@ describe('fail function', () => {
     })
   })
 
-  describe('repair function', () => {
+  describe('Enhancer functions succeed', () => {
+    
+    const expectedItem = {
+        name: "Broad Sword",
+        durability: 100,
+        enhancement: 0
+    }
+
     it('exists', () => {
       expect(repair).toBeDefined()
     })
+    it("item is repaired to 100 durability", () => {
+        expect(enhancer.repair(item)).toEqual(expectedItem)
+    })
+    it("durability is repaired even when parameter object contains other key value pairs", () => {
+        expect(enhancer.repair(item2)).not.toStrictEqual(expectedItem)
+        expect(enhancer.repair(item2).durability).toStrictEqual(expectedItem.durability)
+    })
+
+    it("return as a new object with a durability rating above 0", () => {
+        expect(enhancer.repair(item)).toBeTruthy()
+        expect(enhancer.repair(item).durability).toBeTruthy()
+    })
   })
+
+  describe("enhancement is successful", () => {
+
+    const expectedItem = {
+        name: "Broad Sword",
+        durability: 0,
+        enhancement: 1
+    }
+
+    it("Adds +1 to enhancement on success", () => {
+        expect(enhancer.succeed(item).enhancement).toEqual(item.enhancement + 1)
+        expect(enhancer.succeed(item)).toEqual(expectedItem)
+    })
+
+    it("enhancement is increased even when parameter object contains other key value pairs", () => {
+        expect(enhancer.succeed(item2)).not.toStrictEqual(expectedItem)
+        expect(enhancer.succeed(item2).enhancement).toStrictEqual(expectedItem.enhancement)
+    })
+
+    it("item is returned as a new object with a enhancement rating above 0", () => {
+        expect(enhancer.succeed(item)).toBeTruthy()
+        expect(enhancer.succeed(item).enhancement).toBeTruthy()
+    })
+
+})
 
   describe('get function', () => {
     it('exists', () => {
